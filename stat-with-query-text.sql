c665b20968c72c441a9acba3db69944271bcc222
J.E. R <5433476+je-r@users.noreply.github.com>
2022-11-01 15:24:46 +0100

    Create stat-with-query-text.sql

---
+++ stat-with-query-text.sql
@@ -0,0 +1,5 @@
+SELECT *   
+FROM sys.dm_exec_cached_plans AS cp
+CROSS APPLY sys.dm_exec_sql_text(plan_handle) AS st
+CROSS APPLY sys.dm_exec_query_plan_stats(plan_handle) AS qps
+--WHERE st.text LIKE 'SELECT * FROM something.something%';
